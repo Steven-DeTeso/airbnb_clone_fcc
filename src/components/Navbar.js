06d136bb26439b20bfd7fecf791f680f31ed9d1a
Steven DeTeso <sadeteso@gmail.com>
2023-03-09 16:40:16 -0500

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar () {
+    return (
+        <nav>
+            <img src={require("../images/airbnb.png")} alt="airbnb logo" />
+        </nav>
+    )
+}
+
+export default Navbar
